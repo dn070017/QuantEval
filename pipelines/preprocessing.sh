155af8c296f3b906208d4af14fd4e46e7f37d119
dn070017 <dn070017@gmail.com>
2017-11-19 19:41:56 +0800

    preprocessing.sh: modify for real data

--- pipelines/preprocessing.sh
+++ pipelines/preprocessing.sh
@@ -7,34 +7,47 @@ SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASEDIR="$SCRIPTDIR/../"
 TRIMMOMATIC="$HOME/bin/Trimmomatic-0.36/trimmomatic-0.36.jar"
 
-for SPECIES in 'mouse' 'dog' 'yeast'
+#for TYPE in 'real' 'simulation'
+for TYPE in 'real'
 do
-    if [ $SPECIES == 'mouse' ]
+    if [ $TYPE == 'real' ]
     then
-        #DEPTHS=( "25x" "50x" "100x" )
-        DEPTHS=( "50x" )
+        READ="raw"
     else
-        DEPTHS=( "50x" )
+        READ="flux_simulator"
     fi
-
-    for DEPTH in "${DEPTHS[@]}"
+    
+    #for SPECIES in 'mouse' 'dog' 'yeast'
+    for SPECIES in 'yeast'
     do
-        SIMDIR=$BASEDIR/simulation/${SPECIES}_${DEPTH}/simulation/
-        READDIR=$BASEDIR/simulation/${SPECIES}_${DEPTH}/reads 
-        mkdir -p $READDIR/fastqc
-        ln $SIMDIR/flux_simulator_r*.fastq $READDIR
-        fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/flux_simulator_r*.fastq
+        if [ $TYPE == 'simulation' ] && [ $SPECIES == 'mouse' ]
+        then
+            #SURFFIXES=( "_25x" "_50x" "_100x" )
+            SURFFIXED=( "_50x" )
+        elif [ $TYPE == 'simulation' ]
+        then
+            SURFFIXES=( "_50x" )
+        else
+            SURFFIXES=( "" )
+        fi
+        
+        for SURFFIX in "${SURFFIXES[@]}" 
+        do
+            READDIR=$BASEDIR/$TYPE/$SPECIES$SURFFIX/reads
+            mkdir -p $READDIR/fastqc
+            fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/${READ}_r*.fastq
 
-        java -jar $TRIMMOMATIC PE -threads $THREADS -phred33 $READDIR/flux_simulator_r1.fastq $READDIR/flux_simulator_r2.fastq $READDIR/read_1.fastq $READDIR/unpaired_read_1.fastq $READDIR/read_2.fastq $READDIR/unpaired_read_2.fastq SLIDINGWINDOW:4:20 MINLEN:30
-        fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/read_*.fastq
+            java -jar $TRIMMOMATIC PE -threads $THREADS -phred33 $READDIR/${READ}_r1.fastq $READDIR/${READ}_r2.fastq $READDIR/read_1.fastq $READDIR/unpaired_read_1.fastq $READDIR/read_2.fastq $READDIR/unpaired_read_2.fastq SLIDINGWINDOW:4:20 MINLEN:30
+            fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/read_*.fastq
 
-        REFDIR=$BASEDIR/reference/$SPECIES
-        MRNADIR=$BASEDIR/simulation/${SPECIES}_${DEPTH}/mRNA
-        mkdir -p $MRNADIR/bwa
-        ln $REFDIR/mRNA.fasta $MRNADIR
-        ln $REFDIR/mRNA.fasta $MRNADIR/bwa
-        bwa index -a is -p $MRNADIR/bwa/mRNA $MRNADIR/bwa/mRNA.fasta
-        bwa mem -t $THREADS $MRNADIR/bwa/mRNA $READDIR/read_1.fastq $READDIR/read_2.fastq | samtools sort -@ $THREADS -O BAM -o $MRNADIR/bwa/bwa.bam - 
-        samtools stats $MRNADIR/bwa/bwa.bam > $MRNADIR/bwa/stats.txt 
+            REFDIR=$BASEDIR/reference/$SPECIES
+            MRNADIR=$BASEDIR/simulation/$SPECIES/mRNA
+            mkdir -p $MRNADIR/bwa
+            ln $REFDIR/mRNA.fasta $MRNADIR
+            ln $REFDIR/mRNA.fasta $MRNADIR/bwa
+            bwa index -a is -p $MRNADIR/bwa/mRNA $MRNADIR/bwa/mRNA.fasta
+            bwa mem -t $THREADS $MRNADIR/bwa/mRNA $READDIR/read_1.fastq $READDIR/read_2.fastq | samtools sort -@ $THREADS -O BAM -o $MRNADIR/bwa/bwa.bam - 
+            samtools stats $MRNADIR/bwa/bwa.bam > $MRNADIR/bwa/stats.txt 
+        done
     done
 done
