661a02ef4a9430ce95cea4e7411eb344bae34ff6
dn070017 <dn070017@gmail.com>
2018-03-07 15:58:21 +0800

    preprocessing.sh: add real reads data simulated by rsem

--- pipelines/preprocessing.sh
+++ pipelines/preprocessing.sh
@@ -17,8 +17,7 @@ do
         READ="flux_simulator"
     fi
     
-    #for SPECIES in 'mouse' 'dog' 'yeast'
-    for SPECIES in 'mouse' 'dog'
+    for SPECIES in 'yeast' 'dog' 'mouse'
     do
         if [ $TYPE == 'simulation' ] && [ $SPECIES == 'mouse' ]
         then
@@ -35,19 +34,43 @@ do
         do
             READDIR=$BASEDIR/$TYPE/$SPECIES$SURFFIX/reads
             mkdir -p $READDIR/fastqc
-            fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/${READ}_r*.fastq
+            #fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/${READ}_r*.fastq
 
-            java -jar $TRIMMOMATIC PE -threads $THREADS -phred33 $READDIR/${READ}_r1.fastq $READDIR/${READ}_r2.fastq $READDIR/read_1.fastq $READDIR/unpaired_read_1.fastq $READDIR/read_2.fastq $READDIR/unpaired_read_2.fastq SLIDINGWINDOW:4:20 MINLEN:30
-            fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/read_*.fastq
+            #java -jar $TRIMMOMATIC PE -threads $THREADS -phred33 $READDIR/${READ}_r1.fastq $READDIR/${READ}_r2.fastq $READDIR/read_1.fastq $READDIR/unpaired_read_1.fastq $READDIR/read_2.fastq $READDIR/unpaired_read_2.fastq SLIDINGWINDOW:4:20 MINLEN:30
+            #fastqc -t $THREADS -f fastq -o $READDIR/fastqc --nogroup $READDIR/read_*.fastq
 
             REFDIR=$BASEDIR/reference/$SPECIES
             MRNADIR=$BASEDIR/$TYPE/$SPECIES$SURFFIX/mRNA
-            mkdir -p $MRNADIR/bwa
-            ln $REFDIR/mRNA.fasta $MRNADIR
-            ln $REFDIR/mRNA.fasta $MRNADIR/bwa
-            bwa index -a is -p $MRNADIR/bwa/mRNA $MRNADIR/bwa/mRNA.fasta
-            bwa mem -t $THREADS $MRNADIR/bwa/mRNA $READDIR/read_1.fastq $READDIR/read_2.fastq | samtools sort -@ $THREADS -O BAM -o $MRNADIR/bwa/bwa.bam - 
-            samtools stats $MRNADIR/bwa/bwa.bam > $MRNADIR/bwa/stats.txt 
+            #mkdir -p $MRNADIR/bwa
+            #ln $REFDIR/mRNA.fasta $MRNADIR
+            #ln $REFDIR/mRNA.fasta $MRNADIR/bwa
+            #bwa index -a is -p $MRNADIR/bwa/mRNA $MRNADIR/bwa/mRNA.fasta
+            #bwa mem -t $THREADS $MRNADIR/bwa/mRNA $READDIR/read_1.fastq $READDIR/read_2.fastq | samtools sort -@ $THREADS -O BAM -o $MRNADIR/bwa/bwa.bam - 
+            #samtools stats $MRNADIR/bwa/bwa.bam > $MRNADIR/bwa/stats.txt 
+            
+            if [ $TYPE == 'real' ]
+            then
+                mkdir -p $MRNADIR/rsem_sim
+                #rsem-prepare-reference -p $THREADS --bowtie2 $MRNADIR/mRNA.fasta $MRNADIR/rsem_sim/rsem.index
+                if [ $SPECIES == 'mouse' ]
+                then
+                    STRAND='reverse'
+                else
+                    STRAND='none'
+                fi
+                #rsem-calculate-expression --paired-end --strandedness $STRAND -p $THREADS --bowtie2 --time $READDIR/read_1.fastq $READDIR/read_2.fastq $MRNADIR/rsem_sim/rsem.index $MRNADIR/rsem_sim/rsem > $MRNADIR/rsem_sim/rsem.out 2> $MRNADIR/rsem_sim/rsem.err
+                
+                THETA=`awk 'NR == 3 {print $1}' $MRNADIR/rsem_sim/rsem.stat/rsem.theta`
+                N=`awk 'NR == 1 {print $4}' $MRNADIR/rsem_sim/rsem.stat/rsem.cnt`
+
+                #rsem-simulate-reads $MRNADIR/rsem_sim/rsem.index $MRNADIR/rsem_sim/rsem.stat/rsem.model $MRNADIR/rsem_sim/rsem.isoforms.results $THETA $N $MRNADIR/rsem_sim/rsem_sim
+
+                mv $READDIR/read_1.fastq $READDIR/read_1_fork.fastq
+                mv $READDIR/read_2.fastq $READDIR/read_2_fork.fastq
+
+                ln $MRNADIR/rsem_sim/rsem_sim_1.fq $READDIR/read_1.fastq
+                ln $MRNADIR/rsem_sim/rsem_sim_2.fq $READDIR/read_2.fastq
+            fi
         done
     done
 done
