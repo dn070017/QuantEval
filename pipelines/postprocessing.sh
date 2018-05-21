b2578b4c74e31d3be3047431e3bacd7bf283a104
dn070017 <dn070017@gmail.com>
2018-05-21 15:16:37 +0800

    small revision: postprocessing.sh

--- pipelines/postprocessing.sh
+++ pipelines/postprocessing.sh
@@ -9,44 +9,30 @@ EVALUE="1e-5"
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASEDIR="$SCRIPTDIR/../"
 
-for TYPE in 'real'
+for TYPE in 'real' 'simulation'
 do
     for SPECIES in 'yeast' 'dog' 'mouse'
     do
-        if [ $TYPE == 'simulation' ] && [ $SPECIS == 'mouse' ]
-        then
-            #DEPTHS=( "25x" "50x" "100x" )
-            DEPTHS=( "_50x" )
-        elif [ $TYPE == 'simulation' ]
-        then
-            DEPTHS=( "_50x" )
-        else
-            DEPTHS=( "" )
-        fi
-
-        for DEPTH in "${DEPTHS[@]}"
+        for SEQ in 'mRNA' 'rnaspades' 'trinity' 'transabyss'
         do
-            for SEQ in 'mRNA' 'rnaspades' 'trinity' 'transabyss'
-            do
-                SEQDIR=$BASEDIR/$TYPE/${SPECIES}${DEPTH}/$SEQ
-                MRNADIR=$BASEDIR/$TYPE/${SPECIES}${DEPTH}/mRNA
-                READDIR=$BASEDIR/$TYPE/${SPECIES}${DEPTH}/reads
+            SEQDIR=$BASEDIR/$TYPE/$SPECIES/$SEQ
+            MRNADIR=$BASEDIR/$TYPE/$SPECIES/mRNA
+            READDIR=$BASEDIR/$TYPE/$SPECIES/reads
             
-                #FLMEAN="$(grep 'insert size average:' $MRNADIR/bwa/stats.txt | grep -oP '(\d+\.*\d+)')"
-                #FLSD="$(grep 'insert size standard deviation:' $MRNADIR/bwa/stats.txt | grep -oP '(\d+\.*\d+)')"
+            #FLMEAN="$(grep 'insert size average:' $MRNADIR/bwa/stats.txt | grep -oP '(\d+\.*\d+)')"
+            #FLSD="$(grep 'insert size standard deviation:' $MRNADIR/bwa/stats.txt | grep -oP '(\d+\.*\d+)')"
             
-                mkdir -p $SEQDIR/blastn
-                ln $SEQDIR/${SEQ}.fasta $SEQDIR/blastn
-                makeblastdb -in $SEQDIR/blastn/${SEQ}.fasta -dbtype nucl
-                blastn -db $SEQDIR/blastn/${SEQ}.fasta -query $SEQDIR/${SEQ}.fasta -outfmt 6 -evalue $EVALUE -perc_identity $IDENTITY -out $SEQDIR/blastn/self.tsv 
-                if [ SEQ != 'mRNA' ]
-                then
-                    blastn -db $SEQDIR/blastn/${SEQ}.fasta -query $MRNADIR/mRNA.fasta -outfmt 6 -out $SEQDIR/blastn/mRNA_to_contig.tsv 
-                    blastn -db $MRNADIR/blastn/mRNA.fasta -query $SEQDIR/${SEQ}.fasta -outfmt 6 -out $SEQDIR/blastn/contig_to_mRNA.tsv 
-                    mkdir -p $SEQDIR/transrate
-                    transrate --assembly $SEQDIR/${SEQ}.fasta --threads $THREADS --output $SEQDIR/transrate --left $READDIR/read_1.fastq --right $READDIR/read_2.fastq
-                fi
-            done
+            mkdir -p $SEQDIR/blastn
+            ln $SEQDIR/${SEQ}.fasta $SEQDIR/blastn
+            makeblastdb -in $SEQDIR/blastn/${SEQ}.fasta -dbtype nucl
+            blastn -db $SEQDIR/blastn/${SEQ}.fasta -query $SEQDIR/${SEQ}.fasta -outfmt 6 -evalue $EVALUE -perc_identity $IDENTITY -out $SEQDIR/blastn/self.tsv 
+            if [ SEQ != 'mRNA' ]
+            then
+                blastn -db $SEQDIR/blastn/${SEQ}.fasta -query $MRNADIR/mRNA.fasta -outfmt 6 -out $SEQDIR/blastn/mRNA_to_contig.tsv 
+                blastn -db $MRNADIR/blastn/mRNA.fasta -query $SEQDIR/${SEQ}.fasta -outfmt 6 -out $SEQDIR/blastn/contig_to_mRNA.tsv 
+                mkdir -p $SEQDIR/transrate
+                transrate --assembly $SEQDIR/${SEQ}.fasta --threads $THREADS --output $SEQDIR/transrate --left $READDIR/read_1.fastq --right $READDIR/read_2.fastq
+            fi
         done
     done
 done
