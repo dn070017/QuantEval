013971ed9bc6caa12e435405818dd14c06137c3b
dn070017 <dn070017@gmail.com>
2018-05-21 14:50:46 +0800

    small revision: read_simulation.sh

--- pipelines/read_simulation.sh
+++ pipelines/read_simulation.sh
@@ -13,27 +13,17 @@ export TMP_DIR="$HOME/temp"
 for SPECIES in 'mouse' 'dog' 'yeast'
 do
     REFDIR="$BASEDIR/reference/$SPECIES"
-    if [ $SPECIES == 'mouse' ]
-    then
-        #DEPTHS=( "25x" "50x" "100x" )
-        DEPTHS=( "50x" )
-    else
-        DEPTHS=( "50x" )
-    fi
-
-    for DEPTH in "${DEPTHS[@]}"
-    do
-        SIMDIR=$BASEDIR/simulation/${SPECIES}_${DEPTH}/simulation/
-        READDIR=$BASEDIR/simulation/${SPECIES}_${DEPTH}/reads
-        mkdir -p $SIMDIR
-        cp -r "$REFDIR/chromosome" $SIMDIR
-        cp -r "$REFDIR/flux_simulator_clean.gtf" $SIMDIR/
+    SIMDIR=$BASEDIR/simulation/$SPECIES/simulation/
+    READDIR=$BASEDIR/simulation/$SPECIES/reads
+    
+    mkdir -p $SIMDIR
+    cp -r "$REFDIR/chromosome" $SIMDIR
+    cp -r "$REFDIR/flux_simulator_clean.gtf" $SIMDIR/
+    
+    flux-simulator -p $SIMDIR/flux_simulator.par -l -s -x > $SIMDIR/flux_simulator.out 2> $SIMDIR/flux_simulator.err 
         
-        flux-simulator -p $SIMDIR/flux_simulator.par -l -s -x > $SIMDIR/flux_simulator.out 2> $SIMDIR/flux_simulator.err 
+    $BASEDIR/scripts/split_interleaved_reads.py $SIMDIR/flux_simulator.fastq
         
-        $BASEDIR/scripts/split_interleaved_reads.py $SIMDIR/flux_simulator.fastq
-        
-        mkdir -p $READDIR
-        ln $SIMDIR/flux_simulator_r*.fastq $READDIR
-    done
+    mkdir -p $READDIR
+    ln $SIMDIR/flux_simulator_r*.fastq $READDIR
 done
